360e54b7e4e7ec7cf785ef2789536757a3e5966d
Andrew Farquharson <farquharson.a@gmail.com>
2019-02-10 09:02:03 +0000

    modal with youtube video

--- assets/cactusblossom.js
+++ assets/cactusblossom.js
@@ -13,4 +13,24 @@ $(document).ready(function() {
       return popoverContentTemplate;
     }
   });
+
+  // Youtube video inside modal
+  // See https://stackoverflow.com/questions/18622508/bootstrap-3-and-youtube-in-modal
+  // http://jsfiddle.net/jeremykenedy/h8daS/1/
+  autoPlayYouTubeModal();
+
+  //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+  function autoPlayYouTubeModal() {
+    var trigger = $("body").find('[data-toggle="modal"]');
+    trigger.click(function () {
+      var theModal = $(this).data("target"),
+          videoSRC = $(this).attr("data-theVideo"),
+          videoSRCauto = videoSRC + "?autoplay=1";
+      $(theModal + ' iframe').attr('src', videoSRCauto);
+      $(theModal + ' button.close').click(function () {
+        $(theModal + ' iframe').attr('src', videoSRC);
+      });
+    });
+  }
+
 });
